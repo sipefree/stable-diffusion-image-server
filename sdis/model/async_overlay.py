7819abee7738e55b3bf4a86fd4cf4b64a97a3eeb
Simon Free <sfree@apple.com>
2023-08-23 08:07:42 -0700

    Writing async image import code

--- sdis/model/async_overlay.py
+++ sdis/model/async_overlay.py
@@ -1,6 +1,11 @@
 from redis.commands.json._util import JsonType as JsonType
 from redis.commands.json.path import Path as Path
-from typing import Optional, Union
+from typing import Optional, Union, TypeVar, Generic, Callable, Any
+from concurrent.futures import ProcessPoolExecutor
+import asyncio
+from asyncio.futures import Future
+from tqdm.asyncio import tqdm as tqdm
+from collections.abc import Generator, AsyncIterator, Awaitable
 
 PathLike = Union[str, Path]
 
@@ -30,3 +35,74 @@ class AsyncJSONCommands:
     async def toggle(self, name: str, path: Optional[PathLike] = ...) -> Union[bool, list[Optional[int]]]: ...
     async def strappend(self, name: str, value: str, path: Optional[int] = ...) -> Union[int, list[Optional[int]]]: ...
     async def debug(self, subcommand: str, key: Optional[str] = ..., path: Optional[PathLike] = ...) -> Union[int, list[str]]: ...
+
+Input = TypeVar('Input')
+Output = TypeVar('Output')
+
+class AsyncProcessMap(Generic[Input, Output], AsyncIterator[Output]):
+    """
+    Asynchronous process map implementation.
+    
+    Maps input items to output by applying a function in parallel using process pool.
+    The class is designed to be used as an asynchronous iterator.
+    
+    Args:
+        func: The function to apply to each input item.
+        input: List of input items.
+        max_workers: The maximum number of processes that can be used to execute the calls. 
+                     If None, the number will be the number of processors on the machine.
+        tqdm_kwargs: Additional keyword arguments for tqdm progress bar.
+    """
+    
+    def __init__(self, 
+                 func: Callable[[Input], Output],
+                 input: list[Input],
+                 max_workers: Optional[int] = None,
+                 **tqdm_kwargs: Any):
+        self.func: Callable[[Input], Output] = func
+        self.input = input
+        self.tqdm_kwargs: dict[str, Any] = tqdm_kwargs
+        self.executor = ProcessPoolExecutor(max_workers=max_workers)
+        self.task_iter: Optional[Generator[Awaitable[Output], None, None]]
+        self.is_submitted = False
+    
+    def __aiter__(self):
+        """Returns the async iterator instance."""
+        if self.is_submitted:
+            raise RuntimeError("Cannot iterate over AsyncProcessMap more than once.")
+        return self
+    
+    async def __anext__(self) -> Output:
+        """Yields next processed result or raises StopAsyncIteration when done."""
+        if not self.is_submitted:
+            self.is_submitted = True
+            await self._submit_work()
+        try:
+            if self.task_iter is None:
+                raise StopAsyncIteration
+            try:
+                task = next(self.task_iter)
+                return await task
+            except StopIteration:
+                raise StopAsyncIteration
+        except Exception as e:
+            self.task_iter = None
+            self.executor.shutdown()
+            raise e
+        
+    async def _submit_work(self):
+        """
+        Submit work to the process pool executor.
+        
+        This function will generate a list of futures representing the work to be done.
+        The futures are then wrapped in an async iterator with a tqdm progress bar.
+        """
+        loop = asyncio.get_running_loop()
+        futures: list[Future[Output]] = []
+        for item in self.input:
+            future = loop.run_in_executor(self.executor, self.func, item)
+            futures.append(future)
+        if len(futures) == 0:
+            return
+        self.task_iter = tqdm.as_completed(futures, **self.tqdm_kwargs)
+    
--- sdis/model/images/image_import.py
+++ sdis/model/images/image_import.py
@@ -1,8 +1,57 @@
 from pathlib import Path
+from attrs import define, field
+
+from .image_keys import ImageKeys
+from .image import Image
+from collections.abc import AsyncIterator
+from ..async_overlay import AsyncProcessMap
+
+@define(frozen=True)
+class ImageImportJob:
+    basepath: Path      = field()
+    genpath: Path       = field()
+    image_path: Path    = field()
+    album_key: str      = field()
+
+@define(frozen=True)
+class ImageImportResult:
+    image: Image
+    album_key: str
 
 
-class ImageImport:
-    pass
+class ImageImport(ImageKeys):
+    
+    def __init__(self, basepath: Path, genpath: Path):
+        self.genpath = genpath
+        super().__init__(basepath)
+        
+    def import_image(self, image_path: Path, album_key: str) -> ImageImportResult:
+        pass
+    
+    @classmethod
+    def import_images_async(cls,
+                            jobs: list[ImageImportJob],
+                            max_workers: int = 8,
+                            tqdm_unit: str = 'raster',
+                            tqdm_desc: str = 'Importing images',
+                            tqdm_dynamic_ncols: bool = True,
+                            tqdm_leave: bool = True) -> AsyncIterator[ImageImportResult]:
+        return AsyncProcessMap(
+            func = _task_import_image, 
+            input = jobs,
+            max_workers = max_workers,
+            unit = tqdm_unit,
+            desc = tqdm_desc,
+            dynamic_ncols = tqdm_dynamic_ncols,
+            leave = tqdm_leave
+        )
+
+def _task_import_image(job: ImageImportJob) -> ImageImportResult:
+    importer = ImageImport(
+        basepath = job.basepath,
+        genpath = job.genpath
+    )
+    return importer.import_image(job.image_path, job.album_key)
 
 # ---------------------------------------------------------------------------- #
 #                                   Utilities                                  #
